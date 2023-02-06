b467b04d295b0b0ac51935f20306b669b7c7217a
Juank628 <jsanchez_zunino@yahoo.com>
2023-02-06 16:26:24 -0500

    navigation routes created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Detail from './pages/Detail';
 
 function App() {
   return (
-    <div className="App">
-      App
-    </div>
+    <BrowserRouter>
+      Navbar
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/detail/:ticker" element={<Detail />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
